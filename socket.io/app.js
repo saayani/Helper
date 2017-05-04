43715ea39480a7e872205c7e305b717dfaf1f1ba
qwerty-sayani <qwerty.sayani@gmail.com>
2017-05-05 01:36:55 +0530

    I tried

---
+++ socket.io/app.js
@@ -0,0 +1,28 @@
+	var app = require('express')();
+	var http = require('http').Server(app);
+	var io = require('socket.io')(http);
+	var path = require('path');
+	 
+	// Initialize application with route to root directory)
+	app.get('/', function(req, res){
+	  var express=require('express');
+	  app.use(express.static(path.join(__dirname)));
+ 
+          //Transfers the file at the given <code>path</code>.
+	  res.sendFile(path.join(__dirname, '../chat-application', 'index.html'));
+	});
+	 
+	// Register events on socket connection
+	io.on('connection', function(socket){ 
+	  socket.on('chatMessage', function(from, msg){
+		io.emit('chatMessage', from, msg);
+	  });
+	  socket.on('notifyUser', function(user){
+		io.emit('notifyUser', user);
+	  });
+	});
+	 
+	// Listen application request on port 3000
+	http.listen(3000, function(){
+	  console.log('listening on *:3000');
+	});
